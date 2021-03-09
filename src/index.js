e92ae9a02aac9c67c34649865c125ecc0c8ea944
Ilyas <ilyasdossanov@gmail.com>
2021-03-09 23:59:43 +0600

    feat: write down the code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minValue = 99999;
+  if(array == undefined){
+    return(0);
+  }
+  else{
+  	for (var i = 0; i < array.length; i++) {
+  		if(array[i] < minValue){
+  			minValue = array[i];
+  		}
+  	}
+return(parseInt(minValue));
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof array === "object" && array !== 0){
+    let maxValue = -99999;
+    for(let i = 0; i< array.length; i++){
+      if(maxValue < array[i]){
+        maxValue = array[i]
+      }
+    }
+    return(parseInt(maxValue))
+  }
+  else{
+    return(0);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof array === "object" && array !== 0){
+    let summury = 0;
+    for(let i = 0; i<array.length; i++){
+      summury+=array[i];
+    }
+    return(summury/array.length)
+  }
+  else{
+    return(0);
+  }
 }
